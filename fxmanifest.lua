584c0997495ef56421f63dcbd530cad138ecff9f
TheNeti <139126727+TheNeti@users.noreply.github.com>
2024-06-02 07:13:15 +0200

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,11 @@
-fx_version "cerulean"
-game "gta5"
-lua54 "yes"
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
-author "Neti"
-version "1.0"
+name 'n-blockresol'
+author 'TheNeti'
+version '1.0.0'
+description 'blocking prohibited screen resolutions | 4:3, 5:4'
+repository 'https://github.com/TheNeti/n-blockresol'
 
 client_script "c.lua"
